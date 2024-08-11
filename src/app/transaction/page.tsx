a7934f573879881aad13e9dd33a85fbb67d9cc67
abhishek-01k <abhishek@push.org>
2024-08-12 01:46:11 +0530

    Added transaction steps into a new page

---
+++ src/app/transaction/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+import brian from "@/lib/brian";
+import { useWeb3ModalAccount } from "@web3modal/ethers/react";
+import React, { useState } from "react";
+import { useAccount } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+import { sendTransaction } from "@wagmi/core";
+import { config } from "@/context/web3modal";
+
+const TransactionPage = () => {
+  const [prompt, setPrompt] = useState("");
+
+  const {
+    address,
+    isConnecting,
+    isConnected,
+    isDisconnected,
+    chainId,
+  } = useAccount();
+
+  console.log("Address >>", address);
+
+  const generatePrompt = async () => {
+    const result = await brian.extract({
+      prompt,
+    });
+
+    console.log("Result >>>", result);
+
+    if (!isConnected) {
+      alert("Connect your wallet");
+      return;
+    }
+
+    if (result && address) {
+      const transactionResult = await brian.transact({
+        ...result,
+        address: address,
+        chainId: chainId ? `${chainId}` : `${mainnet.id}`,
+      });
+
+      console.log("Transaction Result:", transactionResult);
+
+      const { data } = transactionResult[0];
+      const { steps } = data;
+      if (steps) {
+        for (const step of steps) {
+          const { from, to, value, data } = step;
+
+          const tx = {
+            from: from,
+            to,
+            value: BigInt(value), // Default to "0" if value is not provided
+            data,
+          };
+          console.log("Tx >>", tx);
+
+          try {
+            const hash = await sendTransaction(config, tx);
+            console.log("Transaction Hash >>", hash);
+          } catch (error) {
+            console.error("Transaction Error >>", error);
+            break; // Stop further transactions if one fails
+          }
+        }
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4 mt-12 ">
+      <div>
+        <div>
+          <input
+            className="px-2 py-1 rounded-md bg-transparent border w-[500px]"
+            value={prompt}
+            onChange={(e) => {
+              setPrompt(e.target.value);
+            }}
+          />
+        </div>
+        <button
+          onClick={generatePrompt}
+          className="border rounded-md px-2 py-1 mt-8"
+        >
+          Genreate
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TransactionPage;
--- src/context/web3modal.tsx
+++ src/context/web3modal.tsx
@@ -3,7 +3,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { cookieStorage, createStorage, State, WagmiProvider } from "wagmi";
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-import { mainnet, sepolia } from "wagmi/chains";
+import { base, mainnet, optimism, sepolia } from "wagmi/chains";
 import { createWeb3Modal } from "@web3modal/wagmi/react";
 
 // 1. Get projectId from https://cloud.walletconnect.com
@@ -18,7 +18,7 @@ const metadata = {
 };
 const queryClient = new QueryClient();
 
-const chains = [mainnet, sepolia] as const;
+const chains = [mainnet, optimism, base] as const;
 export const config = defaultWagmiConfig({
   chains,
   projectId,
